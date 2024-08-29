d32b9f4b08d771457d65ffbbd9646d51de309c78
Oleg Troian <sleite4r@gmail.com>
2024-08-29 10:55:08 +0200

    update vercel.json

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
+    outDir: "dist", // це вказує на те, що зібрані файли будуть у директорії dist
     sourcemap: true,
-    outDir: "dist",
   },
 });
